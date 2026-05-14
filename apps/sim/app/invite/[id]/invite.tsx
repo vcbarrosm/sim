97ddbb5b986af9988c73ecebefd95a8db5f36898
vcbarrosm <64150730+vcbarrosm@users.noreply.github.com>
2026-05-14 12:44:54 -0300

    Update invite.tsx

--- apps/sim/app/invite/[id]/invite.tsx
+++ apps/sim/app/invite/[id]/invite.tsx
@@ -270,7 +270,7 @@ export default function Invite() {
           title="You've been invited!"
           description={
             isNewUser
-              ? 'Create an account to join this workspace on Sim'
+              ? 'Create an account to join this workspace on AInbase.com'
               : 'Sign in to your account to accept this invitation'
           }
           icon='userPlus'
